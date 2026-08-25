d7102038c1b4d64aa01122b1e4a31824b5c16a74
João Gustavo <jgcmendonca@gmail.com>
2026-08-25 16:12:10 -0300

    fix(api): enable cors for frontend requests

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const jsonServer = require('json-server')
+const cors = require('cors')
 
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 const port = process.env.PORT || 3000
+
+server.use(cors())
+server.options('*', cors())
 
 server.use(middlewares)
 server.use(jsonServer.bodyParser)
